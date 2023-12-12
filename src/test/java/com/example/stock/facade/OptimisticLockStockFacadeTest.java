10ec560878371ad9586fdbe2e8c83c5686f7d8ab
SeongjinOliver <yoosngj@gmail.com>
2023-12-12 09:44:52 +0900

    Optimistic Lock, Pessimistic Lock 코드 추가

--- src/test/java/com/example/stock/facade/OptimisticLockStockFacadeTest.java
+++ src/test/java/com/example/stock/facade/OptimisticLockStockFacadeTest.java
@@ -1,7 +1,8 @@
-package com.example.stock.service;
+package com.example.stock.facade;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import com.example.stock.service.PessimisticLockStockService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,11 +17,11 @@ import java.util.concurrent.Executors;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class StockServiceTest {
+class OptimisticLockStockFacadeTest {
 
     @Autowired
-    private PessimisticLockStockService stockService;
-//    @Autowired
+    private OptimisticLockStockFacade optimisticLockStockFacade;
+    //    @Autowired
 //    private StockService stockService;
     @Autowired
     private StockRepository stockRepository;
@@ -33,17 +34,6 @@ class StockServiceTest {
     @AfterEach
     public void after() {
         stockRepository.deleteAllInBatch();
-    }
-
-    @DisplayName("재고감소")
-    @Test
-    public void decreaseStock() {
-        stockService.decrease(1L, 1L);
-
-        // 100 - 1 = 99
-        Stock stock = stockRepository.findById(1L).orElseThrow();
-
-        assertEquals(99, stock.getQuantity());
     }
 
     /**
@@ -61,7 +51,9 @@ class StockServiceTest {
         for (int i = 0; i < threadCount; i++) {
             executorService.submit(() -> {
                 try {
-                    stockService.decrease(1L, 1L);
+                    optimisticLockStockFacade.decrease(1L, 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 } finally {
                     latch.countDown();
                 }
@@ -73,5 +65,4 @@ class StockServiceTest {
         // 100 - (1 * 100 ) = 0
         assertEquals(0, stock.getQuantity());
     }
-
 }
